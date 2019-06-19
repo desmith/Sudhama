7ea0eab45e3cff950418478efdc00bbfbc10f3b1
Dave E Smith <desmith@gmail.com>
2019-06-18 22:55:30 -0400

    tricky bits

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-from .main.ota_updater import OTAUpdater
-from .include.secrets import _ssid, _pass
-#import .main.thingspeak
-from .main.thingspeak import main as ts
-from .main.moisture import readSoilMoisture
-#import moisture
+from ota_updater import OTAUpdater
+#from include.secrets import _ssid, _pass
+#from .main.thingspeak import main as ts
+#from .main.moisture import readSoilMoisture
+#from .main.humidtemp import main as ht
 
-from .main.humidtemp import main as ht
 
 
 # led = Pin(2, Pin.OUT)
 
 def download_and_install_update_if_available():
-     o = OTAUpdater('url-to-your-github-project')
+     o = OTAUpdater('https://github.com/desmith/sudhama')
      o.download_and_install_update_if_available(_ssid, _pass)
 
 
 def start():
-    wetness = readSoilMoisture()
-    ht()
-    ts()
+
+    import main.thingspeak
+    import main.moisture
+    import main.humidtemp
+    wetness = moisture.readSoilMoisture()
+    print("wetness: ", wetness)
+    humidtemp.main()
+    thingspeak.main()
 
 
 def boot():
